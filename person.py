4cdd668d57b15748f12203d785ba035151033c3c
Áron Kékes <kekes.aron@gmail.com>
2016-06-28 14:58:47 +0200

    Person class is ready, tested, and working well

--- person.py
+++ person.py
@@ -1,2 +1,16 @@
 class Person:
-    pass
+    def __init__(self, f_name, l_name, birth_y, gender, mood, energy):
+        self.first_name = f_name
+        self.last_name = l_name
+        self.year_of_birth = birth_y
+        self.gender = gender
+        self.mood = mood
+        self.energy = energy
+
+    def increase_energy(self, delta):
+        self.energy += delta
+        return self.energy
+
+    def increase_mood(self, delta):
+        self.mood += delta
+        return self.mood
