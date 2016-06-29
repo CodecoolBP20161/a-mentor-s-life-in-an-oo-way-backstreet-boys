40858f31184e45c84ae245f1717fcf5544819974
Áron Kékes <kekes.aron@gmail.com>
2016-06-29 10:34:33 +0200

    person.py changed, print added to increase_energy() and increase_mood()

--- person.py
+++ person.py
@@ -1,4 +1,5 @@
 class Person:
+
     def __init__(self, f_name, l_name, birth_y, gender, mood, energy):
         self.first_name = f_name
         self.last_name = l_name
@@ -7,10 +8,28 @@ class Person:
         self.mood = mood
         self.energy = energy
 
+
     def increase_energy(self, delta):
         self.energy += delta
-        return self.energy
+        if delta > 0:
+            print("The energy level of %s %s increased by %i" % (self.f_name, self.l_name, delta)
+        if delta < 0:
+            print("The energy level of %s %s decreased by %i" % (self.f_name, self.l_name, abs(delta))
+        if delta == 0:
+            print("The energy level of %s %s didn't change" % (self.f_name, self.l_name))
+        return delta
+
 
     def increase_mood(self, delta):
         self.mood += delta
-        return self.mood
+        if delta > 0:
+            print("The mood of %s %s increased by %i" % (self.f_name, self.l_name, delta)
+        elif delta < 0:
+            print("The mood of %s %s decreased by %i" % (self.f_name, self.l_name, abs(delta))
+        else:
+            print("The mood of %s %s didn't change" % (self.f_name, self.l_name))
+        return delta
+
+# person = Person("Miki", "Miklos", 1234, "male", 10, 10)
+# person.increase_energy(20)
+# person.increase_mood(20)
